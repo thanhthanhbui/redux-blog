972af3167af7d4d3fbcdfe82471d69547eb0b71b
Thanh Bui <ttthanhbui@gmail.com>
2019-03-24 17:32:56 -0600

    CRUD actions

--- src/reducers/blogs.js
+++ src/reducers/blogs.js
@@ -4,10 +4,22 @@ const blogs = ( state = [], action ) => {
       return action.blogs
     case 'ADD_BLOG':
       return [action.blog, ...state]
-    case 'EDIT_BLOG':
-      return state.map((blog) => blog.id === action.id ? {...blog, editing: !blog.editing} : blog)
     case 'DELETE_BLOG':
       return state.filter((blog) => blog.id !== action.id)
+    case 'EDIT_BLOG':
+      return state.map((blog) => blog.id === action.id ? {...blog, editing: !blog.editing} : blog)
+    case 'UPDATE_BLOG':
+      return state.map((blog) => {
+        if (blog.id === action.id) {
+          return {
+            ...blog,
+            title: action.blog.newTitle,
+            body: action.blog.newBody,
+            editing: !blog.editing
+          }
+        } else
+            return blog
+      })
     default:
       return state
   }
